3a131c8072d03d1a4062f2fecc766a20f2f8ecd5
bgenchel <benjiegenchel@gmail.com>
2017-01-06 20:06:10 -0800

    added new music menu, changed up menu button colors, added side buffers to further bring attention down to the menus, styled the top bar a bit, hover effect for social media buttons

--- js/custom.js
+++ js/custom.js
@@ -6,47 +6,60 @@ $(document).ready(function() {
     //     $(this).src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/293870832&amp;color=#888888&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true";
     // });
 
-    $(window).scroll(function(e) {
-        var musicDiv = $("#music");
-        var projectsDiv = $("#projects");
-        var otherDiv = $("#other");
-        var sectionLinks = $(".section-link");
-        var windowCenter = $(window).scrollTop() + $(window).height()/2;
+    // $(window).scroll(function(e) {
+    //     var musicDiv = $("#music");
+    //     var projectsDiv = $("#projects");
+    //     var otherDiv = $("#other");
+    //     var sectionLinks = $(".section-link");
+    //     var windowCenter = $(window).scrollTop() + $(window).height()/2;
 
-        var link_id;
-        if(musicDiv.position().top <= windowCenter && windowCenter <= projectsDiv.position().top){
-            link_id = "music-link";
-        } else if(projectsDiv.position().top <= windowCenter  && windowCenter <= otherDiv.position().top){
-            link_id = "projects-link";
-        } else if(otherDiv.position().top <= windowCenter){
-            link_id = "other-link";
-        }
-        for(var i = 0; i < sectionLinks.length; i++){
-            if(sectionLinks[i].id == link_id){
-                sectionLinks[i].classList.add("section-link-on");
-            } else {
-                sectionLinks[i].classList.remove("section-link-on");
-            }
-        }
+    //     var link_id;
+    //     if(musicDiv.position().top <= windowCenter && windowCenter <= projectsDiv.position().top){
+    //         link_id = "music-link";
+    //     } else if(projectsDiv.position().top <= windowCenter  && windowCenter <= otherDiv.position().top){
+    //         link_id = "projects-link";
+    //     } else if(otherDiv.position().top <= windowCenter){
+    //         link_id = "other-link";
+    //     }
+    //     for(var i = 0; i < sectionLinks.length; i++){
+    //         if(sectionLinks[i].id == link_id){
+    //             sectionLinks[i].classList.add("section-link-on");
+    //         } else {
+    //             sectionLinks[i].classList.remove("section-link-on");
+    //         }
+    //     }
 
-        if($("#projects-link").hasClass("section-link-on")){
-            $(".project-link").css("background-color", "#344666");
+    //     if($("#music-link").hasClass("section-link-on")){
+    //         $(".song-link").css("background-color", "#344666");
+    //     } else {
+    //         $(".song-link").css("background-color", "#7f3c05");
+    //     }
+
+    //     if($("#projects-link").hasClass("section-link-on")){
+    //         $(".project-link").css("background-color", "#344666");
+    //     } else {
+    //         $(".project-link").css("background-color", "#230254");
+    //     }
+    // });
+
+    $("#music-link").mouseenter(function(e) {
+        $("#music-links").css("display", "inline-block");
+        $("#project-links").css("display", "none");
+        if($("#music-link").hasClass("section-link-on")){
+            $(".song-link").css("background-color", "#344666");
         } else {
-            $(".project-link").css("background-color", "black");
+            $(".song-link").css("background-color", "#7f3c05");
         }
     });
 
-    $("#projects-link").hover(function(e) {
+    $("#projects-link").mouseenter(function(e) {
+        $("#music-links").css("display", "none");
         $("#project-links").css("display", "inline-block");
         if($("#projects-link").hasClass("section-link-on")){
             $(".project-link").css("background-color", "#344666");
         } else {
-            $(".project-link").css("background-color", "black");
+            $(".project-link").css("background-color", "#230254");
         }
-    });
-
-    $("#music-link").mouseenter(function(e) {
-        $("#project-links").css("display", "none");
     });
 
     $("#other-link").mouseenter(function(e) {
@@ -54,6 +67,7 @@ $(document).ready(function() {
     });
 
     $("#section-menu").mouseleave(function(e) {
+        $("#music-links").css("display", "none");
         $("#project-links").css("display", "none");
     });
 });
