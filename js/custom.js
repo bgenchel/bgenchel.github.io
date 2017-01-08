1a63c00e8b601b3b9ba3b53c54c18794ebd1ba9e
bgenchel <benjiegenchel@gmail.com>
2017-01-07 20:22:24 -0800

    navbar scrolls with page

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,18 @@
 
 $(document).ready(function() {
+    var static_nav_line = $("#navbar").position().top;
 
-    // $(document).on("click", "#fly-with-the-wind-frame", function(e) {
-    //     console.log("clicked frame");
-    //     $(this).src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/293870832&amp;color=#888888&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true";
-    // });
+    $(window).scroll(function(e) {
+        if($(window).scrollTop() >= static_nav_line){
+            $("#navbar").addClass("static-navbar");
+            $("#navbar-spacer").height($("#navbar").height());
+        }
 
-    // $(window).scroll(function(e) {
+        if($(window).scrollTop() < static_nav_line){
+            $("#navbar").removeClass("static-navbar");
+            $("#navbar-spacer").height(0);
+        }
+
     //     var musicDiv = $("#music");
     //     var projectsDiv = $("#projects");
     //     var otherDiv = $("#other");
@@ -40,26 +46,16 @@ $(document).ready(function() {
     //     } else {
     //         $(".project-link").css("background-color", "#230254");
     //     }
-    // });
+    });
 
     $("#music-link").mouseenter(function(e) {
         $("#music-links").css("display", "inline-block");
         $("#project-links").css("display", "none");
-        // if($("#music-link").hasClass("section-link-on")){
-        //     $(".song-link").css("background-color", "#344666");
-        // } else {
-        //     $(".song-link").css("background-color", "#ff7700");
-        // }
     });
 
     $("#projects-link").mouseenter(function(e) {
         $("#music-links").css("display", "none");
         $("#project-links").css("display", "inline-block");
-        // if($("#projects-link").hasClass("section-link-on")){
-        //     $(".project-link").css("background-color", "#344666");
-        // } else {
-        //     $(".project-link").css("background-color", "#230254");
-        // }
     });
 
     $("#other-link").mouseenter(function(e) {
